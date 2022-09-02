93dd9303a0e1f6902c757e9a8bb2c2b3e4433213
Guilherme Sandoval <guijpjs@gmail.com>
2022-09-02 10:07:08 -0300

    style :pencil: : Add Global Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Button } from "./components/Button";
 import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes';
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Button />
       <Button />
       <Button />
+      <GlobalStyles />
     </ThemeProvider>
   )
 }
