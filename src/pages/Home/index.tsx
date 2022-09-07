43dd2492d2f579731e1c14e8acbee0c1c37609e4
Guilherme Sandoval <guijpjs@gmail.com>
2022-09-07 01:19:54 -0300

    chore :sparkles:: Create structure homePage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Play } from "phosphor-react";
+import * as S from "./styles";
 
 export function Home() {
   return (
-    <div>Home</div>
+    <S.Container>
+      <form action="">
+        <S.FormContainer>
+          <label htmlFor="nameProject">Vou trabalhar em</label>
+          <input type="text" id="nameProject" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="text" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </S.FormContainer>
+
+        <S.CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <S.Separator>:</S.Separator>
+          <span>0</span>
+          <span>0</span>
+        </S.CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </S.Container>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,7 +9,8 @@ export const GlobalStyles = createGlobalStyle`
 
   body {
     background-color: ${(props) => props.theme["gray-900"]};
-    color: ${(props) => props.theme["gray-300"]}
+    color: ${(props) => props.theme["gray-300"]};
+    --webkit-font-smooting: antialiased;
   }
 
   button {
@@ -24,6 +25,5 @@ export const GlobalStyles = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
   }
 `;
