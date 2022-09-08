2beaaff15a56bc226632b9162d0a9fb258b5cafd
Guilherme Sandoval <guijpjs@gmail.com>
2022-09-08 16:08:17 -0300

    chore :sparkles:: Crete structure page History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import * as S from "./styles";
 
 export function History() {
   return (
-    <div>History</div>
+    <S.Container>
+      <h1>Meu Histórico</h1>
+
+      <S.HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+          </tbody>
+        </table>
+      </S.HistoryList>
+    </S.Container>
   );
 }
