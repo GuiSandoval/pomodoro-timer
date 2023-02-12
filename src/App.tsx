13cb41d4499b0c628b19a6d4a207c26490b978e3
Guilherme Sandoval <guijpjs@gmail.com>
2023-02-12 19:24:11 -0300

    Change BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/themes";
@@ -9,16 +9,19 @@ import { Router } from "./Router";
 import { CyclesContextProvider } from "./contexts/CyclesContext";
 
 export function App() {
-  const baseNameUrl = window.location.pathname.split("/")[1];
+  // using this to deploy.
+  // const baseNameUrl = window.location.pathname.split("/")[1];
 
   return (
-    <BrowserRouter basename={`/${baseNameUrl}`}>
+    // <BrowserRouter basename={`/${baseNameUrl}`}>
+    // Using HashRouter to GitHub Pages.
+    <HashRouter>
       <ThemeProvider theme={defaultTheme}>
         <CyclesContextProvider>
           <Router />
         </CyclesContextProvider>
         <GlobalStyles />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
